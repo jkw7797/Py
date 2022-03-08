eb4a71b06606b39d949c593b591999f20eb86c13
Kwanwoo Jeon <55167417+jkw7797@users.noreply.github.com>
2022-03-08 16:15:31 +0900

    Update 02 구현1.py

--- na/02 구현1.py
+++ na/02 구현1.py
@@ -1,21 +1,41 @@
-#상하좌우
-n = int(input("공간크기"))
-x,y = 1,1
-plans = input().split()
+def sol(n, movePlan):
+    import time
+    start_time = time.time()
 
-dx=[0,0,-1,1]
-dy=[-1,1,0,0]
-move_types = ['L','R','U','D']
+    man = [1, 1]
+    moveR = [0, 1]
+    moveL = [0, -1]
+    moveU = [-1, 0]
+    moveD = [1, 0]
 
-for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
+    for i in movePlan:
+        if i == 'R':
+            man[0] += moveR[0]
+            man[1] += moveR[1]
+        elif i == 'L':
+            man[0] += moveL[0]
+            man[1] += moveL[1]
+        elif i == 'U':
+            man[0] += moveU[0]
+            man[1] += moveU[1]
+        elif i == 'D':
+            man[0] += moveD[0]
+            man[1] += moveD[1]
 
-        if nx < 1 or ny<1 or nx > n or ny > n:
-            continue
+        if man[0] < 1:
+            man[0] = 1
+        elif man[0] > n:
+            man[0] = 5
 
-        x,y = nx,ny
+        if man[1] < 1:
+            man[1] = 1
+        elif man[1] > n:
+            man[1] = 5
+        # print(man)
 
-print(x,y)
+    end_time = time.time()  # 측정 종료
+    print("time: ", end_time - start_time)  # 수행시간 출력
+
+    return man
+print(sol(100,"RRRRRUDDRRRUDDRRRUDDRUDDRRUDDRRRUDDRRRUDDRRRUDDRRRUDDRUDRRRRRUDDRRRUDDRRRUDDRUDDRRUDDRRRUDDRRRUDDRRRUDDRRRUDDRUDDRRRRRUDDRRRUDDRRRUDDRUDDRRUDDRRRUDDRRRUDDRRRUDDRRRUDDRUDDD"))
+
